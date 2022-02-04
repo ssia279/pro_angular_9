ffd10020039ea621211055dd1b4887513d98f030
Johnson Wang <johnson.wang@northstartechlab.com>
2022-02-03 22:52:11 -0800

    complete chapter 9 exercise

---
+++ chap_09/SportsStore/src/app/store/store.component.ts
@@ -0,0 +1,54 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { Cart } from "../model/cart.model";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
+
+@Component({
+  selector: "store",
+  templateUrl: "store.component.html"
+})
+export class StoreComponent {
+  public selectedCategory: string | undefined | null = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
+
+  constructor(private repository: ProductRepository, private cart: Cart, private router: Router) { }
+
+  get products() : Product[] {
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
+  }
+
+  get categories() : (string | undefined)[] {
+    return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string ) {
+    this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number | null) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageCount(): number {
+    return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage);
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
+    this.router.navigateByUrl("/cart");
+  }
+
+  // obsolete method call. This used to be the function to generate page selector. Now we use pageCount().
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
+              .fill(0).map((x, i) => i + 1);
+  }
+}
